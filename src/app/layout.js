e94bf801371675c937dce345c0679533c06c6266
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-01-29 13:50:44 +0530

    added google tags

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,6 +1,7 @@
 import { Geist, Geist_Mono,Roboto,Archivo,Norican } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -29,6 +30,18 @@ const roboto = Roboto({
   variable: "--font-roboto",
 });
 
+const GtaScript = `window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-NNNXV5XK65');`
+
+
+const GtmScript = `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-K32G2W58');`
 
 
 export const metadata = {
@@ -39,8 +52,23 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body
-        className={`${archivo.variable} antialiased`}>
+      <head>
+        <Script>
+          {GtmScript}
+        </Script>
+      </head>
+
+          <Script async src="https://www.googletagmanager.com/gtag/js?id=G-NNNXV5XK65" />
+          <Script>
+            {GtaScript}
+          </Script>
+
+      <body className={`${archivo.variable} antialiased`}>
+        
+          <noscript>
+            <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-K32G2W58" height="0" width="0" style="display:none;visibility:hidden" />
+          </noscript>
+
           <Navbar/>
         {children}
       </body>
