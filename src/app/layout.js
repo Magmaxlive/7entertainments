5ac646fc8fa92f5fdaed12c38bd559981a656c3b
mujeebrahmanik <mujeebrahmanikamaal@gmail.com>
2026-01-29 14:13:20 +0530

    added google tags v3

--- src/app/layout.js
+++ src/app/layout.js
@@ -51,7 +51,7 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="en" suppressHydrationWarning>
       <head>
         <Script>
           {GtmScript}
